415bbab0d52f6e645d7b92f43bd751dfac1403cd
LuizF245 <fernandoalmeidaluiz769@gmail.com>
2021-01-01 05:34:31 -0300

    Tradução para Laravel 8

--- src/pt_BR/auth.php
+++ src/pt_BR/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Essas credenciais não batem com nossos registros.',
+    'failed' => 'Essas credenciais não foram encontradas em nossos registros.',
     'password' => 'A senha fornecida está incorreta.',
     'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
 
